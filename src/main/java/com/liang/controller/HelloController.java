5da175711ba1f57a4d52c2104dba6379d88d6152
liangyehao <1094311509@qq.com>
2020-04-05 23:10:07 +0800

    初始化项目

---
+++ src/main/java/com/liang/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.liang.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author liangyehao
+ * @version 1.0
+ * @date 2020/4/5 22:37
+ * @content
+ */
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello jenkins";
+    }
+}
