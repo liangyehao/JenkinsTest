553bbfeefe508205c9b92dd46ddceeb1b28b8cd1
liangyehao <liangyehao@aliyun.com>
2020-04-06 22:55:59 +0800

    修改判断pid为空条件

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ cp ./target/$APP_NAME /home/deploy/
 
 echo "stopping $APP_NAME ..."
 pid=`ps -ef | grep -w "$APP_NAME" | grep -v "grep" | awk '{print $2}'`
-if [-z $pid ];then
+if [-z "$pid" ];then
  echo "$APP_NAME is not running..."
 else 
  echo "doing kill -9 $pid"
