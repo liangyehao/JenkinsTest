df179f2a0dd61a9c320bfc10cac10ce164d4491c
liangyehao <liangyehao@aliyun.com>
2020-04-06 21:37:01 +0800

    测试脚本自动运行jar包

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-echo "执行部署脚本"
-echo $APP_NAME
+echo "starting ci..."
+echo "APP_NAME is $APP_NAME ..."
+
+echo "copying jar to deploy directory..."
+cp ./target/$APP_NAME /home/deploy/
+
+cd /home/deploy
+
+echo "starting app..."
+nohup java -jar $APP_NAME &
+
+echo "$APP_NAME is running..."
 
