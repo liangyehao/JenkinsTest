b88297ddd47d3b9f5e8dede3a85b1e6e2f7ddd92
liangyehao <liangyehao@aliyun.com>
2020-04-06 22:46:51 +0800

    测试自动关闭应用并重启

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,15 @@ echo "APP_NAME is $APP_NAME ..."
 echo "copying jar to deploy directory..."
 cp ./target/$APP_NAME /home/deploy/
 
+echo "stopping $APP_NAME ..."
+pid=`ps -ef | grep -w "$APP_NAME" | grep -v "grep" | awk '{print $2}'`
+if [-z $pid ];then
+ echo "$APP_NAME is not running..."
+else 
+ echo "doing kill -9 $pid"
+ kill -9 $pid
+fi
+ 
 cd /home/deploy
-
-echo "starting app..."
-nohup java -jar $APP_NAME &
-
-echo "$APP_NAME is running..."
-
+echo "starting $APP_NAME ..."
+nohup java -jar $APP_NAME & echo "$APP_NAME is running..."
